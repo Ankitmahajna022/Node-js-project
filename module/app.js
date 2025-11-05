1c6b121f9affabc7fb6ab60d66bf7f5b72ba6ef4
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-11-05 15:26:23 +0530

    first commit

---
+++ module/app.js
@@ -0,0 +1,25 @@
+// wap to find area of circle,triangle,square,rectangle = math.js export, app.js
+
+const circle=(r)=>{
+     console.log(3.14*r*r)
+}
+
+const triangle=(a,b)=>{
+    console.log((a*b)/2)
+}
+const square=(a)=>{
+     console.log(a*a)
+}
+
+const rectangle=()=>{
+    console.log(w*l)
+}
+
+
+module.exoprts={
+
+    circle:circle,
+    triangle:triangle,
+    square:square,
+    rectangle:rectangle
+}
---
+++ module/math.js
@@ -0,0 +1,9 @@
+const {circle,triangle,square,rectangle} = require("./app")
+
+circle(5)
+
+triangle(2,5)
+
+square(5)
+
+rectangle(5,9)
