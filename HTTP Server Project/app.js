ab4a6137b525da2c9e4fff4ece7bfaa759bf4dc4
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-11-11 00:09:53 +0530

    HTTP project

--- HTTP Server Project/app.js
+++ HTTP Server Project/app.js
@@ -1,12 +1,14 @@
 const fs=require("fs")
 const path=require("path")
 const http=require("http")
-http.createServer((req,res)=>{
+const server=http.createServer((req,res)=>{
     let filePath;
 
-    const log=`[${new Data().toLocaleString()}]${req.url}\n`
 
-    fs.appendFile("logs.txt",log,()=>{})
+     const log = `[${new Date().toLocaleString()}] ${req.url}\n`;
+    fs.appendFile(logFile, log, (err) => {
+        if (err) console.error("Error writing log:", err);
+    });
 
 
     if(req.url==="/")
@@ -17,4 +19,35 @@ http.createServer((req,res)=>{
     {
         filePath="about.html"
     }
+    else if(req.url==="/contact")
+    {
+        filePath="contact.html"
+    }
+    else if(req.url==="data")
+    {
+        res.writeHead(200,{"Content-Type": "application/json"})
+        res.end(JSON.stringify({mssage:"Hello Node.js/data route"}))
+        return
+    }
+    else
+    {
+        filePath="404.html"
+    }
+
+    fs.readFile(path.join(__dirname ,filePath),(err,data)=>{
+        if(err)
+        {
+            res.writeHead(500,{"Content-Type":"text/plain"})
+            res.end("Srever Error")
+        }
+        else
+        {
+             res.writeHead(500,{"Content-Type":"text/html"})
+            res.end(data)
+        }
+    })
 })
+
+server.listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
+});
