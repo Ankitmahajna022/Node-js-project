afbaedd3964063c4b97aade07908cc4f929a4a62
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-11-14 12:07:39 +0530

    http last updata

--- HTTP Server Project/app.js
+++ HTTP Server Project/app.js
@@ -1,52 +1,49 @@
-const fs=require("fs")
-const path=require("path")
-const http=require("http")
-const server=http.createServer((req,res)=>{
+import fs from "fs";
+import path from "path";
+import http from "http";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const logFile = path.join(__dirname, "logs.txt");
+
+const server = http.createServer((req, res) => {
     let filePath;
 
-
-     const log = `[${new Date().toLocaleString()}] ${req.url}\n`;
+    const log = `[${new Date().toLocaleString()}] ${req.url}\n`;
     fs.appendFile(logFile, log, (err) => {
         if (err) console.error("Error writing log:", err);
     });
 
-
-    if(req.url==="/")
-    {
-        filePath="index.html"
-    }
-    else if(req.url==="/about")
-    {
-        filePath="about.html"
-    }
-    else if(req.url==="/contact")
-    {
-        filePath="contact.html"
-    }
-    else if(req.url==="data")
-    {
-        res.writeHead(200,{"Content-Type": "application/json"})
-        res.end(JSON.stringify({mssage:"Hello Node.js/data route"}))
-        return
-    }
-    else
-    {
-        filePath="404.html"
+    if (req.url === "/") {
+        filePath = "/page/index.html";
+    } 
+    else if (req.url === "/about") {
+        filePath = "/page/about.html";
+    } 
+    else if (req.url === "/contact") {
+        filePath = "/page/contact.html";
+    } 
+    else if (req.url === "/data") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Hello Node.js /data route" }));
+        return;
+    } 
+    else {
+        filePath = "/page/404.html";
     }
 
-    fs.readFile(path.join(__dirname ,filePath),(err,data)=>{
-        if(err)
-        {
-            res.writeHead(500,{"Content-Type":"text/plain"})
-            res.end("Srever Error")
+    fs.readFile(path.join(__dirname, filePath), (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Server Error");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
         }
-        else
-        {
-             res.writeHead(500,{"Content-Type":"text/html"})
-            res.end(data)
-        }
-    })
-})
+    });
+});
 
 server.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
