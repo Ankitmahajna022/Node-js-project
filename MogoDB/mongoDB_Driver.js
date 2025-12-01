ebee3504b7be68ad2a7f27d976dd355705362f57
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-12-01 14:29:32 +0530

    MgoDB5

--- MogoDB/mongoDB_Driver.js
+++ MogoDB/mongoDB_Driver.js
@@ -1,31 +1,33 @@
 import { MongoClient } from "mongodb";
 
 
-const client = new MongoClient('mongodb://localhost:27017')
+const client = new MongoClient('mongodb://localhost:27017/')
 
 export const connectDB = async () => {
 
     await client.connect();
     console.log("Connected....")
-    const db = client.db("employee's")
+    const db = client.db("employees")
     return db
 }
 
-export const addEmployee = async () => {
+ const addEmployee = async () => {
 
     const db = await connectDB()
 
-    db.collection("employee's").insertOne({
-        "name": "Rahul Sharma",
-        "employeeId": "EMP001",
-        "department": "Sales",
-        "position": "Sales Executive",
-        "email": "rahul.sharma@example.com",
-        "phone": "+91 9876543210",
-        "salary": 35000,
-        "joiningDate": "2022-06-15",
-        "isActive": true
+    db.collection("employees").insertOne({
+        name: "Rahul Sharma",
+        employeeId: "EMP001",
+        department: "Sales",
+        position: "Sales Executive",
+        email: "rahul.sharma@example.com",
+        phone: "+91 9876543210",
+        salary: 35000,
+        joiningDate: "2022-06-15",
+        isActive: true
     })
 }
 
 addEmployee()
+
+
