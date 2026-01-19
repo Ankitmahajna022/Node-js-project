09c0344c0fca21337e6f40ae53b364ead86218aa
Ankit Mahajan <ankitmahajan2257@gmail.com>
2026-01-19 16:53:18 +0530

    router set

--- Healthcare_Admin_Panel/backend/routes/authRoutes.js
+++ Healthcare_Admin_Panel/backend/routes/authRoutes.js
@@ -1,20 +1,28 @@
 import express from "express"
-import {signin,signup,verifyOtp,changePassword,forgetPassword} from "../controllers/authController.js"
-import {protect} from "../middleware/authMiddleware.js"
+import { 
+  signup,
+  signin,
+  verifyOtp,
+  changePassword,
+  forgetPassword,
+  verifyOtpForCreatePassword,
+  getCurrentUser,
+  logout
+} from "../controllers/authController.js"
+
+import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
-//Auth
 router.post("/signup", signup)
 router.post("/signin", signin)
 router.post("/verify-otp", verifyOtp)
 
-//Password
 router.post("/change-password",changePassword)
-router.post("/forget-password",forgetPassword)
+router.post("/forget-password", forgetPassword)
+router.post("/verify-otp-create-password", verifyOtpForCreatePassword)
 
-//User
-//router.get("/me",protect,getCurrentUser)
-//router.post("",protect,logOut)
+router.get("/me", protect, getCurrentUser)
+router.post("/logout", protect, logout)
 
-export default router
+export default router;
