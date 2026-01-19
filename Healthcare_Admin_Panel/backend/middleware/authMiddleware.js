82e23c254b0cc2affae8bbe60f392281eb443a02
Ankit Mahajan <ankitmahajan2257@gmail.com>
2026-01-19 16:54:03 +0530

    ...

--- Healthcare_Admin_Panel/backend/middleware/authMiddleware.js
+++ Healthcare_Admin_Panel/backend/middleware/authMiddleware.js
@@ -1,23 +1,12 @@
-export const protect = async (req, res, next) => {
-  try {
-    // check if cookies exist
-    if (!req.cookies || !req.cookies.token) {
-      return res.status(401).json({ message: "Not authorized, token missing" })
-    }
+import jwt from "jsonwebtoken"
 
-    const token = req.cookies.token
+export const protect = (req, res, next) => {
+    const token = res.cookies.token
+
+    if (!token) return res.status(401).json({ message: "Unauthorized" })
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    const user = await Auth.findById(decoded.id).select("-password")
-    if (!user) {
-      return res.status(401).json({ message: "User not found" })
-    }
-
-    req.user = user
+    res.user = decoded
     next()
-
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" })
-  }
 }
